1d7e8b9818c68573e46154ea6703244899f85a75
Akeshala <chavinduakeshala@gmail.com>
2023-12-05 23:17:29 +0530

    main method

--- src/main/java/com/akeshala/Main.java
+++ src/main/java/com/akeshala/Main.java
@@ -1,19 +1,19 @@
 package com.akeshala;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import com.akeshala.dto.Location;
+import com.akeshala.factory.LocationFactory;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        ArrayList<Location> locationsList = LocationFactory.createLocations();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        // create an event to handle
+        locationsList.stream()
+                .filter(location -> location.getClosingTime() > 1900 && location.getFee() < 5)
+                .map(Location::getName)
+                .forEach(System.out::println);
     }
 }
