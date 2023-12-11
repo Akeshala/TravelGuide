f49671d964b5487ea6821ddd80cbf86af1b0dbb0
Akeshala <chavinduakeshala@gmail.com>
2023-12-11 18:22:30 +0530

    nit

--- src/main/java/com/akeshala/dto/Museum.java
+++ src/main/java/com/akeshala/dto/Museum.java
@@ -5,5 +5,4 @@ public class Museum extends Location {
     public Museum(String name, Double fee, Integer closingTime, Integer openingTime){
         super(name, fee, closingTime, openingTime);
     }
-
 }
